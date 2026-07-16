e3ddb9e46dbe00a86d3dd4d593160eb484050843
choihoomba <talote33@naver.com>
2026-07-16 14:17:37 +0900

    chore: 서버 실행 시 접속 URL 출력 추가

--- src/server.js
+++ src/server.js
@@ -4,5 +4,6 @@ import env from './config/env.js';
 const PORT = env.PORT;
 
 app.listen(PORT, () => {
-  console.log(`서버가 ${PORT}에서 작동하고 있어요!`);
+  console.log(`서버가 정상적으로 작동하고 있어요! http://localhost:${PORT}`);
+  console.log(`Swagger: http://localhost:${PORT}/api-docs`);
 });
